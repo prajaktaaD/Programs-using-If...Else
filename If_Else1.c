c436ddda02b9215803e13a877d3939299c8b51da
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-04 10:11:23 +0530

    Create If_Else1.c
    1.Find Weather the number is even or odd::

---
+++ If_Else1.c
@@ -0,0 +1,18 @@
+//1.Find Weather the number is even or odd::
+
+#include<stdio.h>
+int main()
+{
+  int num;
+  printf("Enter the number:");
+  scanf("%d",&num);
+  
+  if(num%2==0)
+  printf("\nNum is Even");
+  
+  else
+  printf(("\nNum id Odd");
+  
+  return 0;
+  
+ } 
