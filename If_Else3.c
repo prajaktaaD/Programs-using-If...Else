682c72f05a918f422b814c6d48f8ee1c112575d0
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-05 07:59:48 +0530

    Create If_Else3.c
    C program to find whether a given number is positive or negative:

---
+++ If_Else3.c
@@ -0,0 +1,18 @@
+//3.Find whether given number is positive or negative:
+
+#include<stdio.h>
+int main()
+{
+  int num;
+  printf("\nEnter the integer number:");
+  scanf("%d",&num);
+  
+  if(num<0)
+   printf("The number(%d) is negetive!",num);
+   
+  else
+   printf("The number(%d) is positive!",num);
+   
+   
+   return 0;
+ }  
