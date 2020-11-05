3dd06590ffeda6be888d468854cd2870412f23d5
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-05 08:17:09 +0530

    Create If_Else6.c
     C program to check weather given character is in lower case or in upper case:

---
+++ If_Else6.c
@@ -0,0 +1,18 @@
+//6.Check weather given character is in lower case or in upper case:
+
+#include<stdio.h>
+int main()
+{
+  char ch;
+  printf("Enter the charachter:");
+  scanf("%d",&ch);
+  
+  if(ch>='A'&&ch<='Z')
+    printf("The charachter is in upper case:");
+        
+  else
+     printf("The charachter is in upper case:");
+     
+  return 0;
+} 
+  
