7de7f16919230d6af39ba8684ad4701f2c85d1f7
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-05 08:06:44 +0530

    Create If_Else4.c
     C program to find whether a given character is alphabet or number.

---
+++ If_Else4.c
@@ -0,0 +1,19 @@
+//4.Find whether given character is alphabate or number:
+
+#include<stdio.h>
+int main()
+{
+  char ch;
+  printf("Enter a charecter other than special chararecter:");
+  scanf("%d",&ch);
+  
+  if((ch>='A' && ch<='Z')||(ch>='a'&&ch<='z'))
+   printf("The entered character is alpabate");
+   
+  else
+   printf("The entered character is number");
+   
+  return 0;
+ } 
+   
+  
